b98dd8dc79772927b25cd50cfa6096f5d3805ae7
zmichaels <zmichaels11@gmail.com>
2017-11-28 12:05:18 -0800

    Split project into multiple subprojects
    - made libgraphics a subproject
    - made libaudio a subproject
    - made libengine a subproject
    - made tests/sound a subproject
    - changed build.gradle to be root project build script

---
+++ tests/sound/src/main/cpp/sound.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+
+#include <memory>
+
+#include <GLES3/gl3.h>
+
+#include "audio/sound.hpp"
+#include "graphics/clear_state_info.hpp"
+#include "app.hpp"
+
+struct AppData {
+	std::unique_ptr<audio::sound> sound;
+	graphics::clear_state_info clearState;
+};
+
+void frame(void * pUserData) {
+	static bool runOnce = true;
+	auto pAppData = reinterpret_cast<AppData *> (pUserData);
+
+	if (runOnce) {
+        printf("Vendor: %s\n", glGetString(GL_VENDOR));
+        printf("Renderer: %s\n", glGetString(GL_RENDERER));
+        printf("Version: %s\n", glGetString(GL_VERSION));
+        printf("Extensions: %s\n", glGetString(GL_EXTENSIONS));
+
+        pAppData->sound->play();
+
+        runOnce = false;
+    }
+
+    pAppData->sound->onFrame();
+    graphics::apply(pAppData->clearState);
+}
+
+int main(int argc, char** argv) {
+	application app(640, 480, "Sound Test");
+
+	auto userData = std::make_shared<AppData>();
+
+	userData->sound = std::make_unique<audio::sound>("data/audio/atmono.ogg");
+	userData->clearState.buffers = graphics::clear_buffer::COLOR;
+	userData->clearState.color = {0.0F, 0.7F, 0.3F, 1.0F};
+
+	app.userData = userData;
+	app.start(frame);
+
+	return 0;
+}
+
