cfa3a3577b232300e84c1185ede885e58127c97e
zmichaels <zmichaels11@gmail.com>
2017-12-12 09:50:26 -0800

    Add libgraphics as dependency for libengine

--- libengine/src/main/include/renderer/sprite_layer_info.hpp
+++ libengine/src/main/include/renderer/sprite_layer_info.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
+#include <vector>
+
+#include "renderer/sprite_resource_info.hpp"
+
 namespace renderer {
     struct sprite_layer_info {
-
+        std::vector<renderer::sprite_resource_info> validImages;
+        std::vector<renderer::image> validMasks;
+        unsigned int maxSprites;
+        unsigned int maxColorTransforms;
+        bool streaming;
     };
 }
