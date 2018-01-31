d5cb67962e978ad33de2f191a821f35b414c10c9
zmichaels <zmichaels11@gmail.com>
2018-01-31 13:13:44 -0800

    Rename parallelogram components
    - renamed bottomLeft to lowerLeft

--- libengine/src/main/include/engine/layers/parallelogram.hpp
+++ libengine/src/main/include/engine/layers/parallelogram.hpp
@@ -6,7 +6,7 @@ namespace engine {
     namespace layers {
         template <typename point_component_T = float>
         struct parallelogram {
-            point<point_component_T> upperLeft, upperRight, bottomLeft;
+            point<point_component_T> upperLeft, upperRight, lowerLeft;
         };
     }
 }
