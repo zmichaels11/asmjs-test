cd92ec5fd2d579779d8042548a61e17529ab9c99
zmichaels <zmichaels11@gmail.com>
2018-01-31 13:47:15 -0800

    Include input_assembly_state_info in state.hpp

--- libgraphics/src/main/include/graphics/state.hpp
+++ libgraphics/src/main/include/graphics/state.hpp
@@ -3,6 +3,7 @@
 #include "graphics/hstate/blend_state_info.hpp"
 #include "graphics/hstate/clear_state_info.hpp"
 #include "graphics/hstate/depth_stencil_state_info.hpp"
+#include "graphics/hstate/input_assembly_state_info.hpp"
 #include "graphics/hstate/pixel_store_state_info.hpp"
 #include "graphics/hstate/rasterization_state_info.hpp"
 #include "graphics/hstate/scissor_state_info.hpp"
